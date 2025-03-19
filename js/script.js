1db255c8b20df5e0efa4afd1d79f0c42b4f57760
MikheevaDaria <mikheevadaria5@gmail.com>
2025-03-19 14:51:11 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -18,29 +18,67 @@ document.addEventListener("DOMContentLoaded", () => {
 
 
     const intensiveImg = document.querySelectorAll(".info__places");
+    const textTitle = ["места, которые можно посетить в городе1", "'экскурсии в городе1","рестораны, которые можно посетить1","спа-комплекс1"];
 
-    intensiveImg.forEach((item, index) => {
+    if(intensiveImg.length){
+        intensiveImg.forEach((item, index) => {
 
-        // объявляем переменную intensiveText,выбираем все элементы с классом intensive__description( описания для изображений)
-            const intensiveText = document.querySelectorAll('.info__text');
-        
-        //Когда курсор наводится на изображение (mouseenter):
-            item.addEventListener('mouseenter', () => {
-        //у изображения уменьшаем видимость
-              item.style.opacity = 0.5;
-        //удаляем атрибут hidden и текст становится видимым
-              intensiveText[index].removeAttribute('hidden');
+            // объявляем переменную intensiveText,выбираем все элементы с классом intensive__description( описания для изображений)
+                const intensiveText = document.querySelectorAll('.info__text');
+            
+            //Когда курсор наводится на изображение (mouseenter):
+                item.addEventListener('mouseenter', () => {
+            //у изображения уменьшаем видимость
+                  item.style.opacity = 1;
+            //удаляем атрибут hidden и текст становится видимым
+                  intensiveText[index].removeAttribute('hidden');
+                    });
+            
+            //Когда курсор убираем с изображения (mouseleave):
+                item.addEventListener('mouseleave', () => {
+            //изображение делаем непрозрачным
+                        item.style.opacity = 1;
+            //добавляем атрибут hidden и тест становится видимым
+                  intensiveText[index].setAttribute('hidden', true);
+                    });
                 });
+    } else {
+        console.log('карточек нет')
+    }
+ 
         
-        //Когда курсор убираем с изображения (mouseleave):
-            item.addEventListener('mouseleave', () => {
-        //изображение делаем непрозрачным
-                    item.style.opacity = 1;
-        //добавляем атрибут hidden и тест становится видимым
-              intensiveText[index].setAttribute('hidden', true);
-                });
+
+// =======================================================================
+    // Дополнительные скрипты
+    // Scroll up
+    // Обратите внимание, что в коде выше уже есть слушатель скролла (на следующей практике уберем повторение)
+
+    const scrollUpButton = document.querySelector('.scroll-up');
+
+    if (scrollUpButton) {
+        const windowHeight = document.documentElement.clientHeight; // Определяем высоту видимой части окна браузера
+
+        // Показать кнопку при прокрутке вниз на высоту экрана
+        document.addEventListener('scroll', () => {
+            let scrollPageY = this.scrollY;
+
+            if (scrollPageY >= windowHeight) {
+                scrollUpButton.classList.add('scroll-up--show');
+            } else {
+                scrollUpButton.classList.remove('scroll-up--show');
+            }
+        });
+
+        // Плавная прокрутка наверх при нажатии на кнопку
+        scrollUpButton.addEventListener('click', () => {
+            window.scrollTo({
+                top: 0,
+                behavior: 'smooth'
             });
-        
+        });
+
+    }
+
 
 
 });
