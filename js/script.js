1ea4e68ed7a615c3c8ee99c20aaf10e0e47a6e27
MikheevaDaria <mikheevadaria5@gmail.com>
2025-04-02 15:06:30 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -79,6 +79,105 @@ document.addEventListener("DOMContentLoaded", () => {
 
     }
 
+    const headerMenu = document.querySelector('.header__menu');
+    if (headerMenu){
+            const headerList = headerMenu.querySelector('.menu__list');
+            const menuData = {
+                link1: {
+                    link: 'booking.html',
+                    title: 'БРОНИРОВАНИЕ',
+                },
+                link2: {
+                    link: '#rooms',
+                    title: 'НОМЕРА И ЦЕНЫ',
+                },
+                link3: {
+                    link: '#info0',
+                    title: 'СПЕЦИАЛЬНОЕ ПРЕДЛОЖЕНИЕ',
+                },
+                link4: {
+                    link: '#footer',
+                    title: 'КОНТАКТЫ',
+                },
+                link5: {
+                    link: 'registration.html',
+                    title: 'ВХОД И РЕГИСТРАЦИЯ',
+                }
+            }
+            const createLink = (UrlLink, title) =>{
+                const link = `
+                <li class="menu__item menu__item">
+                                    <a class="menu__link" href="${UrlLink}">${title}</a>
+                </li>
+                `;
+                return link;
+            }
+            for (const linkItem in menuData) {
+                const link = menuData[linkItem];
+                const linkIndex = createLink(link.link, link.title);
+                headerList.insertAdjacentHTML('beforeend', linkIndex);
+    
+            }
+    }    
 
+    const cardsContainer = document.querySelector('#info0');
+    if (cardsContainer) {
+        const cardList = cardsContainer.querySelector('.info__wrap');
+
+        // Пример URL для получения данных с сервера
+        const apiUrl = 'data.json';
+
+        // Функция для создания карточки
+        const createCard = (linkUrl, iconUrl, iconAlt, title, description) => {
+
+            // Шаблонные строки и подстановки
+            const card = `
+     <div class="info__places">
+                <img class="info__icon" src="images/place.jpg" alt="фото мест" width="400" height="350">
+                <a class="info__link" href="${linkUrl}">места, которые
+                    можно посетить в городе</a>
+                <div class="info__text" hidden>
+                    Сбросьте якорь в рутине!
+                    Забудьте о банальных туристических маршрутах и отправляйтесь в увлекательное путшествие по самым душевным и колоритным уголкам города!
+                    Создайте свою уникальную историю города!
+                </div>
+            </div>
+
+
+                <a class="card__item" href="${linkUrl}">
+                    <span class="card__icon">
+                        <img src="${iconUrl}" alt="${iconAlt}">
+                    </span>
+                    <h3 class="card__title">${title}</h3>
+                    <p class="card__description">${description}</p>
+                </a>
+            `;
+
+            return card;
+        }
+
+        // Загрузка данных с сервера
+        fetch(apiUrl)
+            .then(response => response.json())
+            .then(data => {
+                console.log(data); // Данные
+                console.log(typeof (data)); // Тип полученных данных
+
+                // for (const item in data) {
+                //     const card = data[item];
+
+                //     const cardElement = createCard(card.link, card.icon, card.iconAlt, card.iconWidth, card.iconHeight, card.title, card.description);
+                //     cardList.insertAdjacentHTML('beforeend', cardElement);
+                // }
+
+                data.forEach(item => {
+                    const cardElement = createCard(item.link, item.icon, item.iconAlt, item.title, item.description);
+                    cardList.insertAdjacentHTML('beforeend', cardElement);
+                });
+            })
+            .catch(error => {
+                console.error('Ошибка при загрузке данных:', error);
+            });
+    }
 
 });
